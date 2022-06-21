9446e46264bdf64946e0cf7b0dce0ab3549fc0ee
villejuutila <cviju002@edu.xamk.fi>
2022-06-21 12:20:52 +0300

    Native tests included (exp. DailyEntries)

---
+++ src/native-test/kotlin/fi/metatavu/timebank/api/test/NativePersonsTestIT.kt
@@ -0,0 +1,16 @@
+package fi.metatavu.timebank.api.test
+
+import fi.metatavu.timebank.api.test.functional.tests.PersonsTest
+import fi.metatavu.timebank.api.test.functional.resources.TestMySQLResource
+import fi.metatavu.timebank.api.test.functional.resources.TestWiremockResource
+import io.quarkus.test.junit.QuarkusIntegrationTest
+import io.quarkus.test.common.QuarkusTestResource
+
+@QuarkusIntegrationTest
+@QuarkusTestResource.List(
+    QuarkusTestResource(TestMySQLResource::class),
+    QuarkusTestResource(TestWiremockResource::class)
+)
+class NativePersonsTestIT : PersonsTest() {
+
+}
